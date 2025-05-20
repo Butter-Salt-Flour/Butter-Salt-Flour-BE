7f7434695a6b3c4a560a20e2b090438dfe94e30a
Seooooo24 <ahyeungii@gmail.com>
2025-05-20 18:49:00 +0900

    initial settings

---
+++ src/main/java/practice/buttersaltflour/ButterSaltFlourBeApplication.java
@@ -0,0 +1,13 @@
+package practice.buttersaltflour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButterSaltFlourBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButterSaltFlourBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/buttersaltflour/ButterSaltFlourBeApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.buttersaltflour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButterSaltFlourBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
