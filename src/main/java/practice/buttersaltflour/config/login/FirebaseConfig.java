b96355c8ae006014b788b3a8d3f6cef2c9c5d1b3
minko <minye0812@naver.com>
2025-05-22 17:14:44 +0900

    refactor: FileInputStream -> ClassPathResource로 변경

--- src/main/java/practice/buttersaltflour/config/login/FirebaseConfig.java
+++ src/main/java/practice/buttersaltflour/config/login/FirebaseConfig.java
@@ -4,16 +4,19 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+@Slf4j
 @Configuration
 public class FirebaseConfig {
     @PostConstruct
     public void init() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-service-account.json");
+        InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
@@ -21,7 +24,7 @@ public class FirebaseConfig {
 
         if (FirebaseApp.getApps().isEmpty()) {
             FirebaseApp.initializeApp(options);
-            System.out.println(">>> FirebaseApp 초기화 완료");
+            log.info(">>> FirebaseApp 초기화 완료");
         }
     }
 }
