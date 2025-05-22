cfc0ff950833690295ea89b48dee127bebfbf66e
baaamk <brandon55321@gmail.com>
2025-05-23 00:29:24 +0900

    refactor: swagger 추가, final 추가
    AuthController, MemberController, MemberResponse 에 스웨거 추가했습니다.

--- src/main/java/practice/buttersaltflour/auth/controller/AuthController.java
+++ src/main/java/practice/buttersaltflour/auth/controller/AuthController.java
@@ -1,17 +1,37 @@
 package practice.buttersaltflour.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import practice.buttersaltflour.auth.service.AuthService;
+import practice.buttersaltflour.domain.gemini.controller.dto.GeminiResponse;
 import practice.buttersaltflour.member.service.MemberService;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "소셜로그인", description = "Auth request API")
 public class AuthController {
     private final AuthService service;
 
+    @Operation(summary = "소셜 로그인 요청")
+    @ApiResponse(responseCode = "200", description = "회원가입 성공",
+            content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject(
+                            name = "LoginSuccess",
+                            summary = "정상 로그인 응답",
+                            value = "{\"message\": \"login success: abc@gmail.com\"}"
+                    )))
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/api/login")
     public ResponseEntity<String> protectedEndpoint() {
         String resultMessage = service.saveIfNew(); // 비즈니스 로직 위임
--- src/main/java/practice/buttersaltflour/member/controller/MemberController.java
+++ src/main/java/practice/buttersaltflour/member/controller/MemberController.java
@@ -1,20 +1,32 @@
 package practice.buttersaltflour.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import practice.buttersaltflour.auth.entity.CustomPrincipal;
+import practice.buttersaltflour.domain.gemini.controller.dto.GeminiResponse;
 import practice.buttersaltflour.member.controller.dto.MemberResponse;
 import practice.buttersaltflour.member.service.MemberService;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "MEMBER 정보", description = "member request API")
 public class MemberController {
 
     private final MemberService service;
-
+    @Operation(summary = "본인 정보 요청 API")
+    @ApiResponse(responseCode = "200", description = "Gemini 응답 성공",
+            content = @Content(schema = @Schema(implementation = MemberResponse.class)))
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/member")
     public ResponseEntity<MemberResponse> findMember(@AuthenticationPrincipal CustomPrincipal customPrincipal) {
         String uid = customPrincipal.getUid();
