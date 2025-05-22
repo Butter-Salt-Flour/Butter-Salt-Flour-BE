b25d73ece87c7a7933bdc14f8c797eb281620a26
minko <minye0812@naver.com>
2025-05-22 15:49:07 +0900

    chore: remove unused import

--- src/main/java/practice/buttersaltflour/member/controller/MemberController.java
+++ src/main/java/practice/buttersaltflour/member/controller/MemberController.java
@@ -1,19 +1,9 @@
 package practice.buttersaltflour.member.controller;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
-import com.google.firebase.auth.UserRecord;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import practice.buttersaltflour.member.entity.Member;
 import practice.buttersaltflour.member.service.MemberService;
 
 @RestController
