6d1fde1c11eef8308e4f7963e85782880d876aa4
baaamk <brandon55321@gmail.com>
2025-05-23 16:15:30 +0900

    refactor: 중복 예외 파일 삭제

--- src/main/java/practice/buttersaltflour/auth/exception/InvalidTokenTypeException.java
+++ src/main/java/practice/buttersaltflour/auth/exception/InvalidTokenTypeException.java
@@ -1,8 +1,9 @@
 package practice.buttersaltflour.auth.exception;
 
+import util.execption.BusinessBaseException;
 import util.execption.ErrorCode;
 
-public class InvalidTokenTypeException extends MemberException {
+public class InvalidTokenTypeException extends BusinessBaseException {
     public InvalidTokenTypeException() {
         super(ErrorCode.FIREBASE_TOKEN_INVALID_TYPE);
     }
